1b5932101e87009150948084713b1d2796cdd6dd
Tanner Rogalsky <tanner@tannerrogalsky.com>
2019-05-29 15:09:17 -0400

    no std rust

---
+++ src/lib.rs
@@ -0,0 +1,30 @@
+#![no_std]
+
+extern crate wasm_bindgen;
+
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+pub fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+// mod utils;
+
+// use wasm_bindgen::prelude::*;
+
+// // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+// // allocator.
+// #[cfg(feature = "wee_alloc")]
+// #[global_allocator]
+// static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+// #[wasm_bindgen]
+// extern {
+//     fn alert(s: &str);
+// }
+
+// #[wasm_bindgen]
+// pub fn greet() {
+//     alert("Hello, wasm-micro-test2!");
+// }
