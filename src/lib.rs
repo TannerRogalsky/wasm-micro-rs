178defd0e18297d70edfa6067140de290a41198c
Tanner Rogalsky <tanner@tannerrogalsky.com>
2019-05-30 16:21:35 -0400

    use smaller allocator

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,12 @@
 #![no_std]
 
+extern crate wee_alloc;
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
 extern crate wasm_bindgen;
 
-use wasm_bindgen::prelude::*;
+use wasm_bindgen::prelude::wasm_bindgen;
 
 #[wasm_bindgen]
 pub fn add(a: i32, b: i32) -> i32 {
@@ -13,23 +17,3 @@ pub fn add(a: i32, b: i32) -> i32 {
 pub fn sum(slice : &[i32]) -> i32 {
     slice.iter().sum()
 }
-
-// mod utils;
-
-// use wasm_bindgen::prelude::*;
-
-// // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-// // allocator.
-// #[cfg(feature = "wee_alloc")]
-// #[global_allocator]
-// static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
-// #[wasm_bindgen]
-// extern {
-//     fn alert(s: &str);
-// }
-
-// #[wasm_bindgen]
-// pub fn greet() {
-//     alert("Hello, wasm-micro-test2!");
-// }
