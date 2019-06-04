85b4d41f5dcfbff6ac70dde6212d39e265b5724e
Tanner Rogalsky <tanner@tannerrogalsky.com>
2019-06-04 15:24:37 -0400

    when weealloc is too big

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,29 @@
 #![no_std]
-
-extern crate wee_alloc;
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
 extern crate wasm_bindgen;
 
 use wasm_bindgen::prelude::wasm_bindgen;
+use core::alloc::{GlobalAlloc, Layout};
+
+extern {
+    static mut __heap_base: u8;
+}
+static mut BUMP_POINTER : isize = 0;
+
+struct BadAllocator;
+
+unsafe impl GlobalAlloc for BadAllocator {
+    unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
+        let r : *mut u8 = (&mut __heap_base as *mut u8).offset(BUMP_POINTER);
+        BUMP_POINTER += layout.size() as isize;
+        r
+    }
+
+    unsafe fn dealloc(&self, _ptr: *mut u8, _layout: Layout) {
+        // ohno.jpg
+    }
+}
+#[global_allocator]
+static ALLOC: BadAllocator = BadAllocator;
 
 #[wasm_bindgen]
 pub fn add(a: i32, b: i32) -> i32 {
